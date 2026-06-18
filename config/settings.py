6574e49af5041a1bfa4b970cef088ad9be524bef
mbuguam443 <mbuguam443@gmail.com>
2026-06-18 10:45:28 +0300

    Add Procfile and CSRF_TRUSTED_ORIGINS for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -7,6 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-dev-key-change-in-production')
 DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='', cast=Csv())
 
 INSTALLED_APPS = [
     'django.contrib.admin',
