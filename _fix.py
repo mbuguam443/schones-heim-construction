ef037b16ebc2e03d2503df94e39934a13d71e172
mbuguam443 <mbuguam443@gmail.com>
2026-07-29 16:23:23 +0300

    Cleanup temp fix files

--- _fix.py
+++
@@ -1,7 +0,0 @@
-# fix it
-with open('apps/core/views.py', 'r') as f:
-    content = f.read()
-content = content.replace("context_object_name = 'houses'", "context_object_name = 'projecthouses'")
-with open('apps/core/views.py', 'w') as f:
-    f.write(content)
-print('Fixed')
