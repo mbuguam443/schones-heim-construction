b98179f72db01471a9bb9ab66a2fd21493bd181d
mbuguam443 <mbuguam443@gmail.com>
2026-07-31 09:53:10 +0300

    Force HTTPS only: SSL redirect, proxy header, HSTS

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,12 @@ if not DEBUG:
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     X_FRAME_OPTIONS = 'DENY'
-    SECURE_SSL_REDIRECT = False  # Set True if SSL cert installed
+    # Force HTTPS only
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
 
 INSTALLED_APPS = [
     'django.contrib.admin',
