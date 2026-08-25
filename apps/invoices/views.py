eb84ab8eb01daf840a15f18e9b875a438395ab2c
mbuguam443 <mbuguam443@gmail.com>
2026-08-25 13:45:23 +0300

    Use newsignature.png for high-res signature on PDFs

--- apps/invoices/views.py
+++ apps/invoices/views.py
@@ -26,7 +26,7 @@ def _logo_data_uri():
 
 
 def _signature_data_uri():
-    path = Path(settings.BASE_DIR) / 'static' / 'signature.png'
+    path = Path(settings.BASE_DIR) / 'static' / 'newsignature.png'
     try:
         data = path.read_bytes()
     except OSError:
